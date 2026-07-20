d16ab86568e99d8687ebbf2d2ab8d6b0d2c9ab6c
huashi33 <huashi33@foxmail.com>
2026-07-20 18:32:06 +0800

    add dic-test

--- test/hcfg_test.c
+++ test/hcfg_test.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include "hcfg.h"
+#include "dictionary.h"
 
-int main(int argc,char *argv[]){
-  const char* cfgname = argv[1];
-  const char* key = argv[2];
+static int dic_test(){
+  int r = 0;
+  dictionary *dic = dictionary_new(0);
+
+  const char *k="name";
+  char vv[256] = "";
+
+  r = dictionary_set(dic,k,"zhangsan");
+  HBASE_RET_WHEN(r,r);
+
+  const char *vget = dictionary_get(dic,k,NULL);
+  HBASE_RET_WHEN(!vget,1);
+  printf("%s:%s\n",k,vget);
 
 
+  return 0;
+}
+static int cfg_test(const char* cfgname,const char* key ){
   hcfg_t c;
   int r = 0;
   r = hcfg_init(&c,cfgname,HCFG_TYPE_INI);
@@ -18,4 +32,12 @@ int main(int argc,char *argv[]){
   r = hcfg_deinit(&c);
   HBASE_RET_WHEN(r, r);
   return 0;
+
+}
+int main(int argc,char *argv[]){
+  int r =0;
+  r = cfg_test(argv[1],argv[2]);
+
+  r = dic_test();
+  return r;
 }
